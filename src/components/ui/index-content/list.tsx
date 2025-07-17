f05b6222f068b760f16bc3ed3462ca87501487b7
Matias Ferraro <matias.agf@gmail.com>
2025-07-17 12:37:24 +1000

    feat: mejorar la lógica de renderizado en ListIndexContent, agregar estilos condicionales y manejo de márgenes

--- src/components/ui/index-content/list.tsx
+++ src/components/ui/index-content/list.tsx
@@ -3,10 +3,13 @@
 import { ENDPOINTS } from "@/lib/constants";
 import { type Article } from "@/lib/interfaces/articles";
 import { useArticles } from "@/lib/providers/articles-provider";
+import { usePathname } from "next/navigation";
 import Link from "next/link";
+import { cn } from "@/lib/utils";
 
 export function ListIndexContent() {
   const { articles: data } = useArticles();
+  const currentEndpoint = usePathname();
 
   const groupByMonth = (items: Article[]) => {
     return Object.groupBy(items, (item) => {
@@ -22,8 +25,24 @@ export function ListIndexContent() {
             <h2 className="font-bold capitalize mb-0">{month}</h2>
             <ul>
               {items?.map((item) => (
-                <li key={item.id} className="text-balance">
-                  <Link href={`${ENDPOINTS.ARTICLE(item.slug)}`}>{item.title}</Link>
+                <li
+                  key={item.id}
+                  className={cn("text-balance")}
+                  style={{
+                    marginLeft:
+                      currentEndpoint === ENDPOINTS.ARTICLE(item.slug) ? "1rem" : "initial",
+                  }}>
+                  <Link
+                    className="transition-colors no-underline"
+                    style={{
+                      fontWeight:
+                        currentEndpoint === ENDPOINTS.ARTICLE(item.slug) ? "bold" : "normal",
+                      textDecoration:
+                        currentEndpoint === ENDPOINTS.ARTICLE(item.slug) ? "none" : "underline",
+                    }}
+                    href={`${ENDPOINTS.ARTICLE(item.slug)}`}>
+                    {item.title}
+                  </Link>
                 </li>
               ))}
             </ul>
