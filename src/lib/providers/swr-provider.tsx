91d8bbadb8dfcf509b72cda7f8ab3b081d8ace40
Matias Ferraro <matias.agf@gmail.com>
2025-09-14 19:13:19 +1000

    fix: aumentar tiempo de revalidación

--- src/lib/providers/swr-provider.tsx
+++ src/lib/providers/swr-provider.tsx
@@ -5,7 +5,7 @@ import { SWRConfig } from "swr";
 export function SWRProvider({ children }: { children: React.ReactNode }) {
   return (
     <SWRConfig
-      value={{ revalidateOnFocus: false, provider: () => new Map(), dedupingInterval: 60_000 }}>
+      value={{ revalidateOnFocus: false, provider: () => new Map(), dedupingInterval: 900_000 }}>
       {children}
     </SWRConfig>
   );
