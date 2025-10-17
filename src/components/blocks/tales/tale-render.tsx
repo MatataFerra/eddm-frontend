2b362b0adfff81fd298d7cebf691cb603e382d13
Matias Ferraro <matias.agf@gmail.com>
2025-10-18 00:05:13 +1000

    minors UI fix

--- src/components/blocks/tales/tale-render.tsx
+++ src/components/blocks/tales/tale-render.tsx
@@ -16,16 +16,16 @@ export function TaleRender({ slug }: { slug: string }) {
           <>
             <div className="max-w-full">
               {tale && tale.header && (
-                <header className="group/header grid grid-cols-1 grid-rows-1 items-center justify-items-center min-h-96 max-h-96 mb-8">
+                <header className="group/header grid grid-cols-1 grid-rows-1 items-center justify-items-center min-h-96 max-h-96 m-8">
                   <h1 className="col-start-1 col-end-2 row-start-1 row-end-auto p-4 z-10 self-center text-4xl text-white font-bold">
                     {tale.title}
                   </h1>
                   <Image
                     src={tale?.header?.url}
                     width={1280}
-                    height={738}
+                    height={100}
                     alt={tale.slug}
-                    className="w-full h-80 opacity-40 -z-10 col-start-1 col-end-2 row-start-1 rounded-2xl row-end-auto group-hover/header:h-96 transition-all duration-300 group-hover/header:rounded-lg group-hover/header:opacity-70 object-cover "
+                    className="w-full h-80 opacity-40 -z-10 col-start-1 col-end-2 row-start-1 rounded-2xl row-end-auto group-hover/header:h-96 transition-all duration-300 group-hover/header:rounded-lg group-hover/header:opacity-70 object-cover aspect-video object-center"
                   />
                 </header>
               )}
