a91980fbdacdf98453f1d7af648d8062a985c1ab
Matias Ferraro <matias.agf@gmail.com>
2025-11-01 09:41:11 +1000

    fix items order and layout on mobile

--- src/components/blocks/tales/list.tsx
+++ src/components/blocks/tales/list.tsx
@@ -5,9 +5,11 @@ import { useTales } from "@/lib/providers/tales-provider";
 export function ListTales() {
   const { tales } = useTales();
   return (
-    <ul className="grid grid-cols-2 gap-4 w-full md:max-w-1/2 mx-auto space-y-4 justify-center">
+    <ul className="grid grid-cols-1 md:grid-cols-2 gap-4 w-full md:max-w-1/2 mx-auto p-4 justify-center">
       {tales && tales.length > 0 ? (
-        tales?.map((tale) => <ListItem key={tale.id} tale={tale} />)
+        tales
+          ?.toSorted((a, b) => a.order - b.order)
+          .map((tale) => <ListItem key={tale.id} tale={tale} />)
       ) : (
         <div className="text-center text-gray-500">No hay relatos disponibles</div>
       )}
