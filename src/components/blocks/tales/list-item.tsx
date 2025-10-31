abd00b400db8746c13986b47b58f0bc9a5a51fe7
Matias Ferraro <matias.agf@gmail.com>
2025-10-31 14:54:31 +1000

    changing layout of tales

--- src/components/blocks/tales/list-item.tsx
+++ src/components/blocks/tales/list-item.tsx
@@ -14,21 +14,21 @@ export function ListItem({ tale: { slug, title, header } }: ListItemProps) {
     <li
       onClick={() => router.push(`/relatos/${slug}`)}
       className={cn(
-        "relative w-full min-h-40 font-poppins font-semibold text-2xl rounded-lg p-4 cursor-pointer transform transition-all duration-300 hover:shadow-md hover:shadow-slate-900 hover:-translate-y-1 hover:translate-x-0.5 overflow-hidden group"
+        "relative w-full min-h-60 font-poppins font-semibold text-2xl rounded-lg p-4 cursor-pointer transform transition-all duration-300 hover:shadow-md hover:shadow-slate-900 hover:-translate-y-1 hover:translate-x-0.5 overflow-hidden group"
       )}>
       <div
         style={{
           backgroundImage: `url('${header?.url}')`,
         }}
-        className="absolute inset-0 bg-cover bg-no-repeat bg-right opacity-50 group-hover:opacity-90 transition-opacity duration-300 z-40 -mask-linear-50 mask-linear-from-40% mask-linear-to-80%"></div>
+        className="absolute inset-0 bg-cover bg-no-repeat bg-right opacity-80 group-hover:opacity-90 transition-opacity duration-300 z-40 -mask-linear-50 mask-linear-from-40% mask-linear-to-80%"></div>
 
       {/* Capa intermedia - Degradado */}
       <div
         className={cn(
-          "absolute inset-0 opacity-80 dark:opacity-90 z-30",
+          "absolute inset-0 opacity-30 dark:opacity-40 z-30",
           "bg-gradient-to-br from-gray-900 to-gray-600"
         )}></div>
-      <div className="relative z-50 h-full flex items-center rounded-md bg-gradient-to-r from-slate-500 to-yellow-100 text-black w-fit px-2 py-1.5 backdrop-blur-sm">
+      <div className="relative z-50 h-fit flex items-center rounded-md bg-gradient-to-r from-slate-500 to-yellow-100 text-black w-fit px-2 py-1.5 backdrop-blur-sm">
         {title}
       </div>
     </li>
--- src/components/blocks/tales/list.tsx
+++ src/components/blocks/tales/list.tsx
@@ -5,7 +5,7 @@ import { useTales } from "@/lib/providers/tales-provider";
 export function ListTales() {
   const { tales } = useTales();
   return (
-    <ul className="flex flex-col w-full md:max-w-1/2 mx-auto space-y-4 justify-center">
+    <ul className="grid grid-cols-2 gap-4 w-full md:max-w-1/2 mx-auto space-y-4 justify-center">
       {tales && tales.length > 0 ? (
         tales?.map((tale) => <ListItem key={tale.id} tale={tale} />)
       ) : (
