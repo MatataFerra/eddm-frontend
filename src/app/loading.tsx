9b2bdb4203edb946168355ebf42826d5bd22fd0b
Matias Ferraro <matias.agf@gmail.com>
2025-11-09 16:39:36 +1000

    fix loading

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,6 +1,7 @@
 export default function Loading() {
   return (
-    <div className="min-h-[60vh] flex items-center justify-center px-6">
+    <div className="min-h-screen flex items-center justify-center px-6">
+      <div className="h-12 w-1/2 bg-slate-500 my-8 mx-auto z-2  p-8 animate-pulse"></div>
       <div className="w-full md:max-w-1/2 mx-auto p-4">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4 justify-center w-full">
           {Array.from({ length: 4 }).map((_, i) => (
