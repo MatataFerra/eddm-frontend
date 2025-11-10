68a84f307ecffb14b58bf2f07a5fe601474c47de
Matias Ferraro <matias.agf@gmail.com>
2025-11-10 20:53:13 +1000

    move suspense to page.tsx

--- src/app/relatos/page.tsx
+++ src/app/relatos/page.tsx
@@ -1,8 +1,10 @@
 import { Nav } from "@/components/blocks/share/nav";
 import { ListTales } from "@/components/blocks/tales/list";
+import { ListSkeleton } from "@/components/blocks/tales/tale-skeleton";
 import { getContentNavigateTales } from "@/lib/api_methods/get-tales";
 import type { ApiResponse } from "@/lib/fetch/caller";
 import type { ContentNavigate } from "@/lib/interfaces/articles";
+import { Suspense } from "react";
 
 export default async function Page() {
   const talesPromise = getContentNavigateTales<ApiResponse<ContentNavigate[]>>();
@@ -12,7 +14,9 @@ export default async function Page() {
       <h1 className="text-5xl my-8 lg:text-7xl font-bold text-center text-white z-2 font-sans ">
         Relatos y escritos
       </h1>
-      <ListTales talesPromise={talesPromise} />
+      <Suspense fallback={<ListSkeleton />}>
+        <ListTales talesPromise={talesPromise} />
+      </Suspense>
     </>
   );
 }
--- src/components/blocks/tales/list.tsx
+++ src/components/blocks/tales/list.tsx
@@ -1,8 +1,7 @@
 import { ListItem } from "@/components/blocks/tales/list-item";
 import { ApiResponse } from "@/lib/fetch/caller";
 import { ContentNavigate } from "@/lib/interfaces/articles";
-import { Suspense, use } from "react";
-import { ListSkeleton } from "./tale-skeleton";
+import { use } from "react";
 
 export function ListTales({
   talesPromise,
@@ -12,16 +11,14 @@ export function ListTales({
   const data = use(talesPromise);
   const tales = data?.data;
   return (
-    <Suspense fallback={<ListSkeleton />}>
-      <ul className="grid grid-cols-1 md:grid-cols-2 gap-4 w-full md:max-w-1/2 mx-auto p-4 justify-center">
-        {tales && tales.length > 0 ? (
-          tales
-            ?.toSorted((a, b) => a.order - b.order)
-            .map((tale) => <ListItem key={tale.id} tale={tale} />)
-        ) : (
-          <div className="text-center text-gray-500">No hay relatos disponibles</div>
-        )}
-      </ul>
-    </Suspense>
+    <ul className="grid grid-cols-1 md:grid-cols-2 gap-4 w-full md:max-w-1/2 mx-auto p-4 justify-center">
+      {tales && tales.length > 0 ? (
+        tales
+          ?.toSorted((a, b) => a.order - b.order)
+          .map((tale) => <ListItem key={tale.id} tale={tale} />)
+      ) : (
+        <div className="text-center text-gray-500">No hay relatos disponibles</div>
+      )}
+    </ul>
   );
 }
